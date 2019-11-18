9ade1595e69fce008b5f0db7069f8dea42006de4
Alfonso Jiménez <me@alfie.cat>
2019-11-18 12:22:22 +0100

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Agata.Mixfile do
   def project do
     [
       app: :agata,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
